1cafc7f5531503652f4a6e8d220bc7cf374d8ec0
Роман Сякаев <r.sjakaev@reg.ru>
2024-01-21 20:03:29 +0400

    Adds vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import handlebars from 'vite-plugin-handlebars';
+import viteInspect from 'vite-plugin-inspect';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist'
+  },
+  plugins: [
+    viteInspect(),
+    handlebars(),
+  ],
+});
