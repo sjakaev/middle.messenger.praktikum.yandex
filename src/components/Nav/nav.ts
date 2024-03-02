749b60975436c121f89a1547935f2ef02911b20e
Роман Сякаев <r.sjakaev@reg.ru>
2024-03-02 19:28:16 +0100

    Fix netify building

--- src/components/Nav/nav.ts
+++
@@ -1,8 +0,0 @@
-import Block from '../../core/Block.ts';
-import template from './template.ts';
-
-export default class Nav extends Block {
-    render() {
-        return this.compile(template);
-    }
-}
