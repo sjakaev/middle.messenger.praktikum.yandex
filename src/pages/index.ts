817e95bfc0a134466ae7a39cf6605125447a74c3
Роман Сякаев <r.sjakaev@reg.ru>
2024-03-02 19:12:53 +0100

    Fix building

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,6 @@
 export { default as ChatPage } from './chat/Chat.ts';
-export { default as RegisterPage } from './register/Register.ts';
-export { default as UserSettingsPage } from './userSettings/UserSettings.ts';
-export { default as Error500Page } from './Error500/Error500.ts';
-export { default as Error404Page } from './Error404/Error404.ts';
-export { default as LoginPage } from './login/Login.ts';
+export { default as RegisterPage } from './register/register.ts';
+export { default as UserSettingsPage } from './userSettings/userSettings.ts';
+export { default as Error500Page } from './error500/error500.ts';
+export { default as Error404Page } from './error404/error404.ts';
+export { default as LoginPage } from './login/login.ts';
