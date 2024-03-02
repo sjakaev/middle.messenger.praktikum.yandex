88875743471e0550c110f848158bddeb55e0454e
Роман Сякаев <r.sjakaev@reg.ru>
2024-03-02 18:57:52 +0100

    Fix building

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,5 @@
 export { default as ChatPage } from './chat/Chat.ts';
-export { default as LoginPage } from './login/ogin.ts';
+export { default as LoginPage } from './login/Login.ts';
 export { default as RegisterPage } from './register/Register.ts';
 export { default as Error404Page } from './Error404/Error404.ts';
 export { default as Error500Page } from './Error500/Error500.ts';
