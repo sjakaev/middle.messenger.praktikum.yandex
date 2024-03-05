f89dbee981be88cb7665cba694e953ce5e9aeba1
Роман Сякаев <r.sjakaev@reg.ru>
2024-03-05 23:36:28 +0400

    Remove ChatItem

--- src/pages/chat/ChatItem/ChatItem.ts
+++
@@ -1,8 +0,0 @@
-import Block from '../../../core/Block.ts';
-import template from './template.ts';
-
-export default class ChatItem extends Block {
-    render() {
-        return this.compile(template, this._props);
-    }
-}
