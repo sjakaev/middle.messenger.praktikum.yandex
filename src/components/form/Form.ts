688265365adb6da72f0d0533246f55570656d9f0
Роман Сякаев <r.sjakaev@reg.ru>
2024-03-05 14:15:53 +0400

    Adds interface for Form

--- src/components/form/Form.ts
+++ src/components/form/Form.ts
@@ -1,8 +1,30 @@
 import template from './template.ts';
 import Block from '../../core/Block.ts';
+import {
+    IInput, IButton,
+} from '../index.ts';
 
 export interface IForm {
     name?: string;
+    inputEmail: IInput,
+    inputLogin: IInput,
+    inputFirstName: IInput,
+    inputSecondName: IInput,
+    inputPhone: IInput,
+    inputPassword: IInput,
+    inputRepeatPassword: IInput,
+    inputChatSearc: IInput,
+    inputSendMessag: IInput,
+    userSettingsMail: IInput,
+    userSettingsLogin: IInput,
+    userSettingsFirstName: IInput,
+    userSettingsSecondName: IInput,
+    userSettingsDisplayName: IInput,
+    userSettingsPhoneNumber: IInput,
+    buttonSaveUserSettings :IButton,
+    buttonSendMessage: IButton,
+    buttonRegister: IButton,
+    buttonSignIn: IButton,
 }
 
 export default class Form extends Block<IForm> {
