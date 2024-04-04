2ee37cd210be856447eec04eb378b470554a3287
Роман Сякаев <r.sjakaev@reg.ru>
2024-04-04 22:02:24 +0400

    Server settings

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.use('/*', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    // eslint-disable-next-line no-console
+    // eslint-disable-next-line
     console.log(`Example app - http://localhost:${PORT}/`);
 });
